40272ad4d7cd35f75156d420cd8e578f10e8d08e
Chris <unixdev73@gmail.com>
2025-12-24 20:12:36 +0100

    Refactor render engine: clean up optimal GPU selection

--- src/renderEngine/device.cpp
+++ src/renderEngine/device.cpp
@@ -19,130 +19,125 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #include "internals.hpp"
+#include "device.hpp"
 
 namespace re {
-Result selectOptimalGPU(RenderEngineT *const engine) {
-  uint32_t count{};
+std::vector<VkPhysicalDevice> queryDevices(VkInstance instance) {
   std::vector<VkPhysicalDevice> devs{};
-
-  std::unordered_map<VkPhysicalDevice, std::vector<VkExtensionProperties>>
-      devExtProps{};
-  std::unordered_map<VkPhysicalDevice, VkPhysicalDeviceFeatures> devFeats{};
-  std::unordered_map<VkPhysicalDevice, VkPhysicalDeviceProperties> devProps{};
-  std::unordered_map<VkPhysicalDevice, std::vector<VkQueueFamilyProperties>>
-      devQFamProps{};
-
-  struct QueueInfo {
-    uint32_t famIndex{};
-    uint32_t count{};
-  };
-
-  struct QueueInfoDB {
-    QueueInfo graphics{};
-    QueueInfo present{};
-  };
-
-  std::unordered_map<VkPhysicalDevice, QueueInfoDB> devQFams{};
-  auto const instance = engine->instance.handle.get();
+  uint32_t count{};
 
   vkEnumeratePhysicalDevices(instance, &count, 0);
   if (!count)
-    return Result::ErrorNoVulkanDevicesAvailable;
+    return {};
 
   devs.resize(count);
   vkEnumeratePhysicalDevices(instance, &count, devs.data());
 
+  return devs;
+}
+
+DeviceInfoT queryDeviceInfo(VkPhysicalDevice_T *const handle) {
+  DeviceInfoT info{};
+  uint32_t count{};
+
+  vkEnumerateDeviceExtensionProperties(handle, 0, &count, 0);
+  info.exts.resize(count);
+  vkEnumerateDeviceExtensionProperties(handle, 0, &count, info.exts.data());
+
+  vkGetPhysicalDeviceFeatures(handle, &info.feats);
+  vkGetPhysicalDeviceProperties(handle, &info.props);
+
+  vkGetPhysicalDeviceQueueFamilyProperties(handle, &count, 0);
+  info.queues.resize(count);
+  vkGetPhysicalDeviceQueueFamilyProperties(handle, &count, info.queues.data());
+  return info;
+}
+
+std::unordered_map<VkPhysicalDevice, DeviceInfoT>
+queryEligibleDevices(VkInstance const instance) {
+  std::unordered_map<VkPhysicalDevice, DeviceInfoT> devInfo{};
+  auto devs = queryDevices(instance);
+
   for (auto device : devs) {
-    count = 0;
-    std::vector<VkExtensionProperties> props{};
-    vkEnumerateDeviceExtensionProperties(device, 0, &count, 0);
-    props.resize(count);
-    vkEnumerateDeviceExtensionProperties(device, 0, &count, props.data());
-    devExtProps.emplace(device, std::move(props));
-
-    VkPhysicalDeviceFeatures feats{};
-    vkGetPhysicalDeviceFeatures(device, &feats);
-    devFeats.emplace(device, std::move(feats));
-
-    VkPhysicalDeviceProperties devProperties{};
-    vkGetPhysicalDeviceProperties(device, &devProperties);
-    devProps.emplace(device, std::move(devProperties));
-
-    std::vector<VkQueueFamilyProperties> qFamProps{};
-    vkGetPhysicalDeviceQueueFamilyProperties(device, &count, 0);
-    qFamProps.resize(count);
-    vkGetPhysicalDeviceQueueFamilyProperties(device, &count, qFamProps.data());
-    devQFamProps.emplace(device, std::move(qFamProps));
-  }
-
-  for (auto device : devs) {
-    QueueInfo graphics{}, present{};
     bool graphicsFound = false, presentFound = false;
+    auto info = queryDeviceInfo(device);
 
-    for (std::size_t i = 0; i < devQFamProps.at(device).size(); ++i) {
-      auto const &qp = devQFamProps.at(device)[i];
-      if (!graphicsFound && qp.queueCount &&
-          qp.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-        graphics.count = qp.queueCount;
-        graphics.famIndex = i;
-        graphicsFound = true;
-      }
+    for (std::size_t i = 0; i < info.queues.size(); ++i) {
       if (graphicsFound && presentFound)
         break;
+
+      if (!graphicsFound && info.queues[i].queueCount &&
+          info.queues[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        graphicsFound = true;
+
       if (presentFound)
         continue;
+
       if (glfwGetPhysicalDevicePresentationSupport(instance, device, i) ==
-          GLFW_TRUE) {
-        present.count = qp.queueCount;
-        present.famIndex = i;
+          GLFW_TRUE)
         presentFound = true;
-      }
     }
 
     if (graphicsFound && presentFound)
-      devQFams.emplace(device, QueueInfoDB{graphics, present});
+      devInfo.emplace(device, std::move(info));
   }
 
-  VkPhysicalDevice currentBest = devQFams.begin()->first;
-  for (auto const &[device, db] : devQFams) {
-    auto itMaxSize = devProps.at(device).limits.maxImageDimension2D;
-    auto curMaxSize = devProps.at(currentBest).limits.maxImageDimension2D;
-    auto const &itFeats = devFeats.at(device);
-    if (itMaxSize > curMaxSize && itFeats.wideLines == VK_TRUE)
-      currentBest = device;
+  return devInfo;
+}
+
+std::unordered_map<VkPhysicalDevice, DeviceInfoT>::const_iterator
+selectOptimalDevice(
+    std::unordered_map<VkPhysicalDevice, DeviceInfoT> const &devs) {
+  auto currentBest = devs.begin();
+  for (auto it = devs.begin(); it != devs.end(); ++it) {
+    auto const cMaxSize = currentBest->second.props.limits.maxImageDimension2D;
+    auto const itMaxSize = it->second.props.limits.maxImageDimension2D;
+    auto const &itFeats = it->second.feats;
+
+    if (itMaxSize > cMaxSize && itFeats.wideLines == VK_TRUE)
+      currentBest = it;
   }
 
-  auto const &qdb = devQFams.at(currentBest);
-  VkDeviceCreateInfo devInfo{};
-  devInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-  devInfo.queueCreateInfoCount =
-      (qdb.graphics.famIndex == qdb.present.famIndex) ? 1 : 2;
+  return currentBest;
+}
 
-  static char const *extensions[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
-  devInfo.enabledExtensionCount = sizeof(extensions) / sizeof(extensions[0]);
-  devInfo.ppEnabledExtensionNames = extensions;
+Result selectOptimalGPU(RenderEngineT *const engine) {
+  auto devs = queryEligibleDevices(engine->instance.handle.get());
+  if (!devs.size())
+    return Result::ErrorNoVulkanDevicesAvailable;
+
+  auto const &[phy, devInfo] = *selectOptimalDevice(devs);
+
+  VkDeviceCreateInfo devCreateInfo{};
+  devCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+  devCreateInfo.queueCreateInfoCount =
+      (devInfo.graphicsQueue.famIndex == devInfo.presentQueue.famIndex) ? 1 : 2;
+
+  static char const *exts[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+  devCreateInfo.enabledExtensionCount = sizeof(exts) / sizeof(exts[0]);
+  devCreateInfo.ppEnabledExtensionNames = exts;
 
   std::vector<VkDeviceQueueCreateInfo> qCreateInfos;
   static const float prio = 1.f;
   VkDeviceQueueCreateInfo q{};
   q.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
   q.queueCount = 1;
-  q.queueFamilyIndex = qdb.graphics.famIndex;
+  q.queueFamilyIndex = devInfo.graphicsQueue.famIndex;
   q.pQueuePriorities = &prio;
   qCreateInfos.push_back(q);
 
-  if (qdb.graphics.famIndex != qdb.present.famIndex) {
-    q.queueFamilyIndex = qdb.present.famIndex;
+  if (devInfo.graphicsQueue.famIndex != devInfo.presentQueue.famIndex) {
+    q.queueFamilyIndex = devInfo.presentQueue.famIndex;
     qCreateInfos.push_back(q);
   }
+  devCreateInfo.pQueueCreateInfos = qCreateInfos.data();
 
-  devInfo.pQueueCreateInfos = qCreateInfos.data();
   VkPhysicalDeviceFeatures reqF{};
   reqF.wideLines = VK_TRUE;
-  devInfo.pEnabledFeatures = &reqF;
+  devCreateInfo.pEnabledFeatures = &reqF;
 
   VkDevice dev{};
-  if (auto result = vkCreateDevice(currentBest, &devInfo, 0, &dev);
+  if (auto result = vkCreateDevice(phy, &devCreateInfo, 0, &dev);
       result != VK_SUCCESS)
     return Result::ErrorVulkanDeviceCreationFailure;
 
@@ -150,10 +145,10 @@ Result selectOptimalGPU(RenderEngineT *const engine) {
   auto &presentQ = engine->device.presentation;
   vkGetDeviceQueue(dev, qCreateInfos.front().queueFamilyIndex, 0, &graphicsQ);
   presentQ = graphicsQ;
-  if (qdb.graphics.famIndex != qdb.present.famIndex)
+  if (devInfo.graphicsQueue.famIndex != devInfo.presentQueue.famIndex)
     vkGetDeviceQueue(dev, qCreateInfos.back().queueFamilyIndex, 0, &presentQ);
 
-  engine->device.identifier = currentBest;
+  engine->device.identifier = phy;
   engine->device.handle = {dev, [](VkDevice ptr) {
                              vkDeviceWaitIdle(ptr);
                              vkDestroyDevice(ptr, 0);
