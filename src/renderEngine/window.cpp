ecb40b6ba16441113031209e2f039158fa4cd081
Chris <unixdev73@gmail.com>
2025-12-26 10:17:56 +0100

    Rename main render engine private header file

--- src/renderEngine/window.cpp
+++ src/renderEngine/window.cpp
@@ -19,7 +19,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #include <badline/renderEngine.hpp>
-#include "internals.hpp"
+#include "engine.hpp"
 #include "instance.hpp"
 #include "device.hpp"
 #include "window.hpp"
